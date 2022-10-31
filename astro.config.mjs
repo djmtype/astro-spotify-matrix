6baa94d6d78de25f05f283d0bcb910625018ac81
Scott Rod <djmtype@gmail.com>
2022-10-31 11:08:24 -0400

    replace with json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// import vue from "@astrojs/vue";
+
+// https://astro.build/config
+import compressor from "astro-compressor";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-spotify-maxtrix',
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    // vue(), 
+    compressor()
+  ]
 });
